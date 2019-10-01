13851201c29421875f94170bd6266ca143a42e36
Terence30 <55691306+Terence30@users.noreply.github.com>
2019-10-01 23:03:56 +0200

    Correction exo16

--- exo_16.rb
+++ exo_16.rb
@@ -1,10 +1,9 @@
 puts "saisir votre année de naissance"
 annee = gets.to_i
 nb_annee= 2019-annee.to_i
-age= 2019-annee.to_i
 diff_annee = 2019-annee.to_i
-
 age=0
+
 nb_annee.times do
     annee += 1
     age += 1
