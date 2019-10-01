b4a44a97d2b8b7ba290708411eb455a9b259bb16
Terence30 <terence30360@gmail.com>
2019-10-01 19:50:09 +0200

    ajout commentaire

--- exo_04.rb
+++ exo_04.rb
@@ -1,1 +1,5 @@
-puts "Salut, ça farte ?
+puts "Salut, ça farte ?"
+
+#Envoi l'erreur exo_04.rb:1: unterminated string meets end of file
+#Il manque le guillemet a la fin du string
+#Corrigé pour pouvoir ajouter des commentaires
