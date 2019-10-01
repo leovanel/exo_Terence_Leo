f9b17a3f3d22a3b3f5857973cb0a5018881fc012
Terence30 <terence30360@gmail.com>
2019-10-01 19:47:00 +0200

    ajout commentaire

--- exo_03.rb
+++ exo_03.rb
@@ -1,2 +1,4 @@
-puts "Bonjour, monde !
-#Et avec une voix sexy, ça donne : Bonjour, monde !"
+puts "Bonjour, monde !"
+#puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+#Tranforme la ligne 2 en commentaire
