3fc072a06417a1065bb6ceffe1041df64c165547
Terence30 <terence30360@gmail.com>
2019-10-01 16:07:34 +0200

    Exo 18

--- exo_18.rb
+++ exo_18.rb
@@ -1,9 +1,15 @@
+myTab= ["jojo@jojo.com"]
 nombre=0
+
 	50.times do
     nombre += 1
     if nombre <10
 	puts  "jojo0#{nombre.to_s}@jojo.com"
+	myTab << "jojo0#{nombre.to_s}@jojo.com"
 	else
 	puts "jojo#{nombre.to_s}@jojo.com"
+	myTab << "jojo#{nombre.to_s}@jojo.com"
 	end
 end
+print myTab
+
