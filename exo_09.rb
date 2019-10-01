a30fbe279dd9da44a82f85330a92a968fe4f00bb
Terence30 <terence30360@gmail.com>
2019-10-01 13:17:09 +0200

    Exo 9 et 10

---
+++ exo_09.rb
@@ -0,0 +1,10 @@
+puts "Quel est votre prénom ?"
+user_lastname = gets.chomp
+print "> "
+
+puts "Quel est votre nom ?"
+user_name = gets.chomp
+print "> "
+
+
+puts "Hello, #{user_lastname} #{user_name}!"
---
+++ exo_10.rb
@@ -0,0 +1,5 @@
+puts "Quelle est votre année de naissance ?"
+year = gets.chomp.to_i
+age = 2017 - year
+
+puts "En 2017 vous aviez #{age} ans"
