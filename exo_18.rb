f9acef0dc02ced0a16d1928a1455a29be2cb8f81
Terence30 <terence30360@gmail.com>
2019-10-01 15:42:08 +0200

    Exo 18-2

--- exo_18.rb
+++ exo_18.rb
@@ -1,6 +1,9 @@
-n=0
-50.times do 
-n += 1
-puts "jean.dupont.#{n}@email.fr" 
-
+nombre=0
+	50.times do
+    nombre += 1
+    if nombre <10
+	puts  "jojo0#{nombre.to_s}@jojo.com"
+	else
+	puts "jojo#{nombre.to_s}@jojo.com"
+	end
 end
