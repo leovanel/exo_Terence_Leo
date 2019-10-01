65564092b4769932b55168e2db7253f34d6b7ac4
Terence30 <terence30360@gmail.com>
2019-10-01 19:46:40 +0200

    ajout commentaire

--- exo_02.rb
+++ exo_02.rb
@@ -1,5 +1,5 @@
-puts "Bonjour, monde !
-Et avec une voix sexy, ça donne : Bonjour, monde !"
+puts "Bonjour, monde !"
+puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
 
 #print permet d'afficher ses paramètres sur la sortie standard (la console). On peut lui passer plusieurs arguments, séparés par des virgules, entre parenthèses ou non.
 #On aurait aussi très bien pu utiliser la méthode puts, qui aurait produit le même résultat avec un retour à la ligne.
