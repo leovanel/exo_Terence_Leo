8e98149a31c001a558fabba68ac7fcc99d05398e
leovanel <leovanel>
2019-10-01 14:15:29 +0200

    exo15

---
+++ exo_15.rb
@@ -0,0 +1,11 @@
+puts "saisir votre année de naissance"
+annee = gets.to_i
+nb_annee= 2019-annee.to_i
+age= 2019-annee.to_i
+
+age=0
+nb_annee.times do
+    annee += 1
+    age += 1
+	puts  "#{annee.to_s} #{age.to_s}"
+end
