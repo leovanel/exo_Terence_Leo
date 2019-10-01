8707047b0f0df16616e80987cd7017f71e443895
Terence30 <terence30360@gmail.com>
2019-10-01 14:24:09 +0200

    Exo 16

---
+++ exo_16.rb
@@ -0,0 +1,13 @@
+puts "saisir votre année de naissance"
+annee = gets.to_i
+nb_annee= 2019-annee.to_i
+age= 2019-annee.to_i
+diff_annee = 2019-annee.to_i
+
+age=0
+nb_annee.times do
+    annee += 1
+    age += 1
+    diff_annee -= 1
+	puts  "Il y a #{diff_annee.to_s} ans vous aviez #{age.to_s} ans"
+end
