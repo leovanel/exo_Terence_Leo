ad084ae5ea3f2b41d1dfd25e31ca7875c7b3de13
leovanel <leovanel>
2019-10-01 12:21:00 +0200

    commit7c

---
+++ exo_07_c.rb
@@ -0,0 +1,4 @@
+user_name = gets.chomp
+puts user_name
+
+#le premier programme ecrit la phrase demandant son nom à l'utilisateur et envoye la ligne de saisie à la ligne, le deuxieme permet d'ajouter le chevron pour indiquer qu'on peut repondre, et le troisime affiche juste la ligne de saisie sans les explications prealables
